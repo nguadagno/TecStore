42770d56462a1b0b4fcff47e8d3c1eec5ec254bd
ppatrone <85507943+ppatrone@users.noreply.github.com>
2022-01-31 17:32:44 +0100

    Update Implementazione

---
+++ .implementazione/TecStore1/src/it/unisa/control/RimozioneArticoloServlet.java
@@ -0,0 +1,51 @@
+package it.unisa.control;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import it.unisa.model.GestioneVendita;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/RimozioneArticolo")
+
+public class RimozioneArticoloServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	GestioneVendita model = new GestioneVendita();
+
+	public RimozioneArticoloServlet() {
+		super();
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		if (!request.getSession().getAttribute("tipologiaUtente").toString().equals("1")
+				&& !request.getSession().getAttribute("tipologiaUtente").toString().equals("4")) {
+			request.getSession().setAttribute("errore", "AccessoNonAutorizzato");
+			response.sendRedirect(request.getContextPath() + "/errore.jsp");
+		}
+
+		request.getSession().setAttribute("operazione", "rimozioneArticolo");
+
+		try {
+			if (request.getSession().getAttribute("IDVenditore").toString()
+					.equals(request.getSession().getAttribute("CF").toString())) {
+				model.rimozioneArticolo(request.getSession().getAttribute("IDArticolo").toString());
+				response.sendRedirect(request.getContextPath() + "/successo.jsp");
+			} else
+				response.sendRedirect(request.getContextPath() + "/errore.jsp");
+
+		} catch (SQLException e) {
+			response.sendRedirect(request.getContextPath() + "/errore.jsp");
+		}
+	}
+}
