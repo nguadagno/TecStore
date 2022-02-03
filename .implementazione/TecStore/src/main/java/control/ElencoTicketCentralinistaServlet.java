703670e99922e73169a6c429dd88d7a893446923
nguadagno <ng@gdgn.xyz>
2022-02-04 00:05:04 +0100

    Elenco ticket centralinista

--- .implementazione/TecStore/src/main/java/control/ElencoTicketCentralinistaServlet.java
+++ .implementazione/TecStore/src/main/java/control/ElencoTicketCentralinistaServlet.java
@@ -6,11 +6,13 @@ import java.util.ArrayList;
 
 import Bean.TicketBean;
 import model.GestioneAssistenza;
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 @WebServlet("/elencoTicketCentralinista")
 public class ElencoTicketCentralinistaServlet extends HttpServlet {
@@ -22,27 +24,36 @@ public class ElencoTicketCentralinistaServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		
+
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		GestioneAssistenza model = new GestioneAssistenza();
+		String redirect = "";
 
-		if (!request.getSession().getAttribute("tipologiaUtente").equals("1")) {
+		if (request.getSession().getAttribute("tipologia") == null
+				|| !request.getSession().getAttribute("tipologiaUtente").equals("2")) {
 			request.getSession().setAttribute("errore", "AccessoNonAutorizzato");
-			response.sendRedirect(request.getContextPath() + "/errore.jsp");
+			response.setStatus(403);
+			redirect = "/accessononautorizzato.jsp";
 		}
 
 		request.getSession().setAttribute("operazione", "elencoTicket");
 
 		try {
-			ArrayList<TicketBean> elenco = model
-					.elencoTicketCentralinista(Integer.parseInt(request.getParameter("limit")));
-			request.getSession().setAttribute("elenco", elenco);
-			response.sendRedirect(request.getContextPath() + "/gestioneassistenza.jsp");
+			int limit = 10;
+			if (request.getParameter("limit") != null && !request.getParameter("limit").isEmpty())
+				limit = Integer.parseInt(request.getParameter("limit"));
+			ArrayList<TicketBean> elencoTicket = model.elencoTicketCentralinista(limit);
+			request.getSession(true).setAttribute("elencoTicket", elencoTicket);
+			redirect = "/gestioneassistenza.jsp";
 		} catch (SQLException e) {
-			response.sendRedirect(request.getContextPath() + "/errore.jsp");
+			response.setStatus(500);
+			redirect = "/errore.jsp";
 		}
+
+		RequestDispatcher dd = request.getRequestDispatcher(redirect);
+		dd.forward(request, response);
 	}
 }
---
+++ .implementazione/TecStore/src/main/webapp/gestioneassistenza.jsp
@@ -0,0 +1,62 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+	pageEncoding="ISO-8859-1"
+	import="java.util.ArrayList, Bean.TicketBean, java.util.Enumeration"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+
+<title>Gestione Ticket</title>
+</head>
+<body>
+	<div align="center">
+		<!-- <form action="gestioneassistenza.jsp" type="post"> -->
+		<form action="gestioneassistenza.jsp?tipologia=<%=2%>&" method="post">
+			<select name="limit">
+				<option value="10">10</option>
+				<option value="20">20</option>
+				<option value="50">50</option>
+			</select> <input type="submit">
+		</form>
+
+		<table>
+			<tr>
+				<th>Codice Ticket</th>
+				<th>Tipologia</th>
+				<th>Data ultimo messaggio</th>
+				<th></th>
+			</tr>
+			<%
+			ArrayList<TicketBean> elencoTicket = (ArrayList<TicketBean>) request.getSession().getAttribute("elencoTicket");
+
+			if (elencoTicket == null) {
+			%>
+			<h3>Nessun ticket in attesa di risposta!</h3>
+			<%
+			return;
+			}
+			%>
+
+			<%
+			for (TicketBean ticket : elencoTicket) {
+			%>
+			<tr>
+				<td><%=ticket.getIDTicket()%></td>
+				<td><%=ticket.getTipologia()%></td>
+				<td><%=ticket.getDataUltimoMessaggio()%></td>
+				<td><form action="dettagliTicket" method="post">
+						<input type="hidden" value=<%=ticket.getIDTicket()%>><input
+							type="submit" value="Dettagli">
+					</form></td>
+
+			</tr>
+
+			<%
+			}
+			%>
+
+		</table>
+
+	</div>
+</body>
+</html>
